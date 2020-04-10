406e1bb0f50e33ae49af941bedf27edaeeb7a241
evans kelechi <evanskcee@gmail.com>
2020-04-10 04:42:23 -0700

    Add project files.

---
+++ stackoverflowmodel/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace stackoverflowmodel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new Post();
+
+            Console.WriteLine("this is a post that models a stackoverflow post\n you can create a post, you can upvote and downvote a post\nyou can view your post also");
+            Console.WriteLine();
+
+            //Console.WriteLine("enter the word 'createpost' to craete a post");
+            Console.WriteLine("enter your post: ");
+            var input = Console.ReadLine();
+            user.Title = input;
+            Console.WriteLine();
+
+            Console.WriteLine("enter post description");
+            var describe = Console.ReadLine();
+            user.Description = describe;
+            Console.WriteLine();
+
+            Console.WriteLine("you can upvote your post, enter 'vote' to upvote your post\n enter 'no vote' to downvote your post");
+            var Like = Console.ReadLine();
+            if (Like == "vote")
+              {
+                  user.Upvote();
+              }
+            else if (Like == "note vote")
+              {
+                  user.Downvote();
+              }
+
+            
+
+            Console.WriteLine("congrat you have successfully create a post.\nenter 'print' to print post");
+            var userpost = Console.ReadLine();
+            if (userpost == "print")
+            {
+                user.printpost();
+            }
+
+
+
+        }
+    }
+}
---
+++ stackoverflowmodel/post.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stackoverflowmodel
+{
+    public class Post
+    {
+        public int Votecount { get; private set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedTime { get; set; }
+
+
+
+        public void Upvote()
+        {
+            Votecount++;
+        }
+
+        public void Downvote()
+        {
+            Votecount--;
+        }
+        
+        public void Createpost()
+        {
+            Console.WriteLine("whats is on your mind: ");
+            var userpost = Console.ReadLine();
+        }
+
+        public void printpost()
+        {
+            //Console.WriteLine("date posted: {0}", CreatedTime.ToLongDateString());
+            //Console.WriteLine();
+            Console.WriteLine(Title);
+            Console.WriteLine();
+            Console.WriteLine(Description);
+            Console.WriteLine();
+            Console.WriteLine("votes: {0}", Votecount);
+        }
+
+
+    }
+}
